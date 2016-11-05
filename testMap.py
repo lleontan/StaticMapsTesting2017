ab3301499e1c83bbd2f70b45f135612bd7fa6864
lleontan <lleontan@gmail.com>
2016-11-05 11:54:51 -0700

    things

--- testMap.py
+++ testMap.py
@@ -1,7 +1,10 @@
 import pygame
-import sys
+from pygame.locals import *
+import os,sys
+from PIL import Image
 from io import BytesIO
 import urllib.request
+
 screen = pygame.display.set_mode((700,300))
 
 
@@ -11,23 +14,25 @@ apiKey="&key=AuAaQIpuk55T4X2UIhXfXitbUHHzIJNHlQLK-Y5v5Na_tx5cAz9Fvmw-xUR5oW8T"
 callType="Imagery/Map/Road/"
 sampleLocation="Bellevue%20Washington?mapLayer=TrafficFlow"
 
-
+print(os.getcwd())
 def getImage(location):
     queryString= baseUrl+callType+location+apiKey
     print(queryString)
-    urllib.request.urlretrieve(queryString, "helloWorld.jpg")
-    image1=pygame.image.load("helloWorld.jpg")
-    return img
+    urllib.request.urlretrieve(queryString, "/Users/tanl/git/StaticMapsTesting2017/helloWorlds.png")
+    #image1=Image.open("/Users/tanl/git/StaticMapsTesting2017/helloWorlds.png")
+    image1=pygame.image.load("/Users/tanl/git/StaticMapsTesting2017/helloWorlds.png")
+
+    return image1
 newQuery=getImage(sampleLocation)
 while True:
-    clock.tick(fps)
+    #clock.tick(fps)
     coord=pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
     screen.fill((0,0,0))   
-    screen.blit(newQuery,(0,0))
+    screen.blit(newQuery,(10,10))
     pygame.display.flip()
     pygame.event.pump()
 
